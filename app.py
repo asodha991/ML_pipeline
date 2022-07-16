a47c5e071268a1da057bbc97b9eadab19f27b6ae
yourusername <asodha991@gmail.com>
2022-07-16 23:17:06 +0530

    Custom Execption created

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-
+from housing.logger import logging
+from housing.exception import HousingException
+import sys
 app = Flask(__name__)
 
 @app.route("/", methods = ['GET','POST'])
 def index():
+    try:
+        raise Exception("We are checking custom exception")
+    except Exception as e:
+        housing = HousingException(e, sys)
+        logging.info(housing)
+        logging.info("We are testing logging model")
     return "CI/ CD Pipeline has been executed successfully"
 
 
